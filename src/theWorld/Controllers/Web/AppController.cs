df2567edcb31a78a1add9dcb1bb4eb8aea2c9f8c
Ivan Salamat <ivan.salamat@gmail.com>
2015-11-12 12:47:06 -0700

    Added MVC Controllers
    Added more pages and taghelpers for ease of navigation

---
+++ src/theWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNet.Mvc;
+using System;
+
+namespace theWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+
+}
